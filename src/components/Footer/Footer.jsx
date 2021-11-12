ed27b24c4747952ead5f8d3fb3432aca0c2382a7
chon26909 <59905927+chon26909@users.noreply.github.com>
2021-11-12 23:35:07 +0700

    update footer

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import "./Footer.scss"
+
+const Footer = () => {
+    return (
+        <div className="footer">
+            <div>
+                Movie App
+            </div>
+            <div>
+                @2021, Movie, Inc.
+            </div>
+        </div>
+    )
+}
+
+export default Footer
